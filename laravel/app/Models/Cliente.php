0815f82721a82bd91b0886c7c988e8042b713e8c
Bruno Vargas Tettamanti <brunovt.074@gmail.com>
2023-02-15 13:12:22 -0300

    Laravel: Models/Cliente.php added - /Libs added - /StoreClienteRequest checked

--- laravel/app/Models/Cliente.php
+++ laravel/app/Models/Cliente.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Cliente extends Model
 {
     use HasFactory;
+    
+    protected $primaryKey = 'nro_cliente';       
+    public $timestamps = false;    
+    public $guarded = 'nro_cliente';
+
+    function nombreCompleto(){
+
+        return "$this->apellido $this->nombre";
+
+    }
+
 }
