19d1216fd8a2e7e5ed05c064a0acaa4c38fd3c72
Bruno Vargas Tettamanti <brunovt.074@gmail.com>
2023-02-15 13:25:19 -0300

    Laravel: routes/web.php modified

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ClientesController;
+use App\Http\Requests\StoreClienteRequest;
+use App\Models\Cliente;    
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,29 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+//Usamos el mismo controlador para todas las rutas
+Route::controller(ClientesController::class)->group(function(){
+	
+	//Ruta fichero raiz
+	Route::get('/', "index")->name('home');
+
+	//Ruta a crear cliente
+	Route::get('/clientes/create', 'create')->name('clientes.create');
+
+	//Ruta a persistir nuevo cliente
+	Route::post('/clientes', 'store')->name('clientes.store');
+
+	//Ruta a cliente especifico
+	Route::get('/clientes/{cliente}', 'show')->name('clientes.show');
+
+	//Ruta a updatear
+	Route::patch('/clientes/{cliente}', 'update')->name('clientes.update');
+
+	//Ruta a vista editar
+	Route::get('/clientes/{cliente}/edit', 'edit')->name('clientes.edit');
+	
+	//Ruta a borrar cliente
+	Route::delete('/clientes/{cliente}', 'destroy')->name('clientes.destroy');
+
 });
+
